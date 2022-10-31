8fb07b1037ecb36893b270ef1fe56cc30ed59fbd
nusubbotin <nu.subbotin@ya.ru>
2022-10-31 14:24:15 +0300

    Часть 1. Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        task1();
 
+    }
+
+    private static void task1() {
+        System.out.println("Часть1. Задание 1:");
+
+        PhoneBook phone = new PhoneBook();
+        for (int i = 0; i < 20; i++) {
+            phone.putPhone("Иван Иванов"+i, "895220941"+i);
+        }
+
+        System.out.println(phone);
     }
 }
